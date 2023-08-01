c96b71030c87d73a5f70b74cf7e8f2848f904da1
Crysart <idukrystal@gmail.com>
2023-08-02 00:26:07 +0100

    04 02

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   const studentsInCity = students.filter((student) => student.location === city);
   return studentsInCity.map((student) => {
     const newGrade = newGrades.find((grade) => grade.studentId === student.id);
-    student.grade = newGrade ? newGrade.grade : 'N/A';
+    student.grade = newGrade ? newGrade.grade : 'N/A'; // eslint-disable-line no-param-reassign
     return student;
   });
 }
