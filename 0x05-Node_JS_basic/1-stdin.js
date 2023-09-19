c3783aa9a6a83bed8a5f8cac7633768370802835
Crysart <idukrystal@gmail.com>
2023-09-19 21:39:23 +0100

    cool

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,8 @@
-const readline = require('readline');
-
-console.log('Welcome to Holberton School, what is your name?');
-
-const myInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-myInterface.question('Your name is: ', () => {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
   process.exit();
 });
-
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
