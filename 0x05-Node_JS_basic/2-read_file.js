02610e4454594ccefa17abd11b5761c146978b6b
Crysart <idukrystal@gmail.com>
2023-09-20 00:55:29 +0100

    you

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,50 @@
+function splitBy(column, csv, columnToList) {
+  function Content(name) {
+    this.name = name;
+    this.list = []
+  }
+
+  let contents  = []
+  const lines = csv;
+  const index = lines[0].split(',').indexOf(column);
+  const cIndex = lines[0].split(',').indexOf(columnToList);
+  for(let i = 1; i < lines.length; i++) {
+    row  = lines[i].split(',');
+    columnValue = row[index];
+    content  = contents.find(
+      (content) => content.name == columnValue
+    )
+    if (!content) {
+      content = new Content(columnValue);
+      contents.push(content);
+    }
+    content.list.push(row[cIndex]);
+  }
+  return contents;
+}
+
+
+function countStudents(dbPath) {
+  const fs = require('fs');
+
+  try {
+    const data = fs.readFileSync(dbPath);
+    const students = data.toString().split('\n');
+    console.log(`Number of students: ${students.length - 1}`);
+    const contents = splitBy('field', students, 'firstname');
+    contents.forEach(
+      (content, index) => {
+        n = content.name
+        l = content.list;
+        msg = `Number of students in ${n}: ${l.length}. List: ${l}`
+        console.log(msg);
+      }
+    )
+    
+  }
+  catch(err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents
