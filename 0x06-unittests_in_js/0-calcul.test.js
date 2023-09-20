1a91a40a7fa28191756dce530546b9a41c674441
Crysart <idukrystal@gmail.com>
2023-09-20 20:00:42 +0100

    01 t3

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,18 +5,18 @@ calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   it(
     "returns correct result",
-    () => assert.equal(calculateNumber(1, 3), 4)
+    () => assert.strictEqual(calculateNumber(1, 3), 4)
   );
   it(
     "returns correct result",
-    () => assert.equal(calculateNumber(1, 3.7), 5)
+    () => assert.strictEqual(calculateNumber(1, 3.7), 5)
   );
   it(
     "returns correct result",
-    () => assert.equal(calculateNumber(1.2, 3.7), 5)
+    () => assert.strictEqual(calculateNumber(1.2, 3.7), 5)
   );
   it(
     "returns correct result",
-    () => assert.equal(calculateNumber(1.5, 3.7), 6)
+    () => assert.strictEqual(calculateNumber(1.5, 3.7), 6)
   );
 });
