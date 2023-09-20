0ecea4477ad8ffc7a4a64ada49b89123466a4e9e
Crysart <idukrystal@gmail.com>
2023-09-20 19:40:52 +0100

    02 t1

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,4 +1,4 @@
-assert = require('assert');
+expect = require('chai').expect;
 
 calculateNumber = require('./1-calcul');
 
@@ -6,23 +6,23 @@ describe('calculateNumber', () => {
   describe('type - SUM', () => {
     it(
       "returns correct result",
-      () => assert.equal(calculateNumber('SUM', 1.4, 4.5), 6)
+      () => expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6)
     );
   });
   describe('type - SUBTRACT', () => {
     it(
       "returns correct result",
-      () => assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
+      () => expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equal(-4)
     );
   })
   describe('type - DIVIDE', () => {
     it(
       "returns correct result - b not close to 0",
-      () => assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
+      () => expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2)
     );
     it(
       "returns correct result - b close to 0",
-      () => assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error')
+      () => expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal('Error')
     );
   })
 });
